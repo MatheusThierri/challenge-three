3611f613147a54fbdb607152ad55e11c9481e59c
MatheusThierri <matheusthierri7@gmail.com>
2025-10-02 20:12:10 -0300

    first commit

---
+++ src/main/java/com/devsuperiorschool/challenge_three/ChallengeThreeApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperiorschool.challenge_three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperiorschool/challenge_three/ChallengeThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorschool.challenge_three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
